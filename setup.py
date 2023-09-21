7175dcc1d3e92aaf24c6c6975dcb3a7d000e32a3
zeio <zeionara@gmail.com>
2023-09-21 23:09:14 +0300

    feat(setup): added setup script

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+def get_readme():
+    with open('README.md', 'r', encoding = 'utf-8') as file:
+        return file.read()
+
+
+setup(
+    name = 'raconteur',
+    packages = ['rr'],
+    version = '0.1',
+    license = 'Apache 2.0',
+    description = 'An auxiliary app for simplifying speech synthesis on arbitrary texts',
+    long_description = get_readme(),
+    author = 'Zeio Nara',
+    author_email = 'zeionara@gmail.com',
+    url = 'https://github.com/zeionara/raconteur',
+    keywords = ['speech', 'synthesis'],
+    classifiers = [
+        'Programming Language :: Python :: 3.11'
+    ]
+)
