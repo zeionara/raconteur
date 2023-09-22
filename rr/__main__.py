470dffe15be88aeff0d32fed3d8e68dfac3e75f5
zeio <zeionara@gmail.com>
2023-09-22 21:19:56 +0300

    feat(handle-aneks): added command for handling datasets with anecdotes

--- rr/__main__.py
+++ rr/__main__.py
@@ -1,12 +1,11 @@
+from os import path, makedirs
+from time import time
+
 from click import group, argument, option
+from pandas import read_csv
 
 from .Splitter import Splitter
-
-from .Bark import Bark
 from .RuTTS import RuTTS
-
-from RUTTS import TTS
-from ruaccent import RUAccent
 
 
 @group()
@@ -42,6 +41,49 @@ def say(text: str, max_n_characters: int):
     # splitter = Splitter(max_n_characters)
 
     # print(splitter.split(text))
+
+
+@main.command()
+@option('--source', '-s', help = 'path to the input tsv file with anecdotes', type = str, default = 'assets/anecdotes.tsv')
+@option('--destination', '-d', help = 'path to the output directory with anecdotes', type = str, default = 'assets/anecdotes')
+@option('--max-n-characters', '-c', help = 'max number of characters given to the speech engine at once', type = int, default = None)
+@option('--top-n', '-n', help = 'number of entries to handle', type = int, default = None)
+@option('--offset', '-o', help = 'number of entries in the beginning to skip', type = int, default = None)
+def handle_aneks(source: str, destination: str, max_n_characters: int, top_n: int, offset: int):
+    if not path.isdir(destination):
+        makedirs(destination)
+
+    df = read_csv(source, sep = '\t')
+
+    n_aneks = 0
+
+    # speaker = Bark(artist = 'v2/ru_speaker_6', splitter = Splitter(max_n_characters = 200))
+    speaker = RuTTS(splitter = Splitter(max_n_characters), add_time_to_end = 0.1, length_scale = 1.65)
+
+    start = time()
+
+    for _, row in (
+        (
+            df if offset is None else df.iloc[offset:,]
+        )
+        if top_n is None else
+        (
+            df.iloc[:top_n,] if offset is None else df.iloc[offset:top_n,]
+        )
+    ).loc[:, ('id', 'text')].iterrows():
+        text = row['text']
+
+        speaker.speak(
+            text = text,
+            filename = path.join(destination, f'{row["id"]:08d}.mp3')
+        )
+
+        n_aneks += 1
+
+        print(f'Handled {n_aneks} aneks')
+
+    elapsed = time() - start
+    print(f'Handled {n_aneks} aneks in {elapsed:.5f} seconds ({elapsed / n_aneks:.5f} seconds per anek in average)')
 
 
 if __name__ == '__main__':
