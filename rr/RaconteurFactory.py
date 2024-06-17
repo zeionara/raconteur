7feb73bcbbcfdfec7c7668ff812f61c712467b8f
Dmitrii <zeionara@gmail.com>
2024-06-17 23:48:18 +0300

    fix(salute): restored salute speech support, fixed artist name passing to fs

--- rr/RaconteurFactory.py
+++ rr/RaconteurFactory.py
@@ -22,7 +22,7 @@ class RaconteurFactory:
                     # client_id = env['SALUTE_SPEECH_CLIENT_ID'],
                     # client_secret = env['SALUTE_SPEECH_CLIENT_SECRET'],
                     auth = env['SALUTE_SPEECH_AUTH'],
-                    artist = 'Nec',
+                    artist = artist,
                     splitter = Splitter(4000 if max_n_characters is None else max_n_characters)
                 )
             # case Bark.name:
