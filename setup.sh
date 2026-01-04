87d9101937a837c990aae677074c40039f5334b9
Zeio Nara <zeionara@gmail.com>
2026-01-04 19:02:55 +0300

    feat(setup): added dependencies to the setup script

--- setup.sh
+++ setup.sh
@@ -11,6 +11,8 @@ conda run -n much --no-capture-output pip install music-tag
 conda run -n much --no-capture-output pip install num2words
 conda run -n much --no-capture-output pip install transliterate
 conda run -n much --no-capture-output pip install python-telegram-bot
+conda run -n much --no-capture-output pip install 'russian-text-stresser @ git+https://github.com/Vuizur/add-stress-to-epub'
+conda run -n much --no-capture-output pip install peft
 
 # much deps
 
