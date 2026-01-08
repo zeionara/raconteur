ec517ebd0321aaeb11a9c4551da99445f6bf9cc1
Zeio Nara <zeionara@gmail.com>
2026-01-08 05:12:48 +0300

    fix(env-name): fixed env name in setup.sh, added aiohttp dependency

--- setup.sh
+++ setup.sh
@@ -4,25 +4,26 @@ set -euo pipefail
 
 conda create -n raconteur 'python<3.12' -y
 
-conda run -n much --no-capture-output pip install chatterbox-tts
-conda run -n much --no-capture-output pip install kokoro
-conda run -n much --no-capture-output pip install ipython
-conda run -n much --no-capture-output pip install music-tag
-conda run -n much --no-capture-output pip install num2words
-conda run -n much --no-capture-output pip install transliterate
-conda run -n much --no-capture-output pip install "python-telegram-bot[job-queue]"
-conda run -n much --no-capture-output pip install 'russian-text-stresser @ git+https://github.com/Vuizur/add-stress-to-epub'
-conda run -n much --no-capture-output pip install peft
-conda run -n much --no-capture-output pip install sentencepiece
+conda run -n raconteur --no-capture-output pip install chatterbox-tts
+conda run -n raconteur --no-capture-output pip install kokoro
+conda run -n raconteur --no-capture-output pip install ipython
+conda run -n raconteur --no-capture-output pip install music-tag
+conda run -n raconteur --no-capture-output pip install num2words
+conda run -n raconteur --no-capture-output pip install transliterate
+conda run -n raconteur --no-capture-output pip install "python-telegram-bot[job-queue]"
+conda run -n raconteur --no-capture-output pip install 'russian-text-stresser @ git+https://github.com/Vuizur/add-stress-to-epub'
+conda run -n raconteur --no-capture-output pip install peft
+conda run -n raconteur --no-capture-output pip install sentencepiece
+conda run -n raconteur --no-capture-output pip install aiohttp
 
 # much deps
 
-conda run -n much --no-capture-output pip install beautifulsoup4
+conda run -n raconteur --no-capture-output pip install beautifulsoup4
 
 # much deps, which are required to run much/alternate.sh
 
-conda run -n much --no-capture-output pip install flask
-conda run -n much --no-capture-output pip install google-images-search
+conda run -n raconteur --no-capture-output pip install flask
+conda run -n raconteur --no-capture-output pip install google-images-search
 
 git submodule update --init
 
